83edcdea0b2ea3bf4856c9745dfc628c9627d12f
calvean <garnetsharara@gmail.com>
2023-04-07 18:51:32 +0000

    all

--- 0x16-api_advanced/100-count#.py
+++ 0x16-api_advanced/100-count#.py
@@ -4,14 +4,10 @@ import requests
 
 
 def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """Prints counts of given words found in hot posts of a given subreddit.
-
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
+    """
+    recursive function that queries the Reddit API, parses the title
+     of all hot articles, and prints a sorted count of given keywords
+      (case-insensitive, delimited by spaces
     """
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
